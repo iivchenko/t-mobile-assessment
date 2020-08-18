4cefc21d46614c062e3b323186fc09444d2a3f67
iivchenko <iivchenko@live.com>
2020-08-18 23:00:18 +0200

    Implement Place Order command

---
+++ src/Stroopwafels.Application/Commands/PlaceOrder/PlaceOrderCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Stroopwafels.Application.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Stroopwafels.Application.Commands.PlaceOrder
+{
+    public sealed class PlaceOrderCommand : IRequest<Unit>
+    {
+        public PlaceOrderCustomer Customer { get; set; }
+
+        public IEnumerable<PlaceOrderItem> Items { get; set; }
+    }
+
+    public sealed class PlaceOrderCustomer
+    {
+        public string Name { get; set; }
+
+        public DateTime WishDate { get; set; }
+    }
+
+    public sealed class PlaceOrderItem
+    {
+        public string Supplier { get; set; }
+
+        public StroopwafelType Type { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
---
+++ src/Stroopwafels.Application/Commands/PlaceOrder/PlaceOrderCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Stroopwafels.Application.Domain;
+using Stroopwafels.Application.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stroopwafels.Application.Commands.PlaceOrder
+{
+    public sealed class PlaceOrderCommandHandler : IRequestHandler<PlaceOrderCommand, Unit>
+    {
+        private readonly IEnumerable<IStroopwafelSupplierService> _services;
+
+        public PlaceOrderCommandHandler(IEnumerable<IStroopwafelSupplierService> services)
+        {
+            _services = services;
+        }
+
+        public async Task<Unit> Handle(PlaceOrderCommand command, CancellationToken cancellationToken)
+        {
+            foreach(var group in command.Items.GroupBy(x => x.Supplier))
+            {
+                var supplier = _services.First(x => x.GetName().GetAwaiter().GetResult() == group.Key);
+
+                var order = new Order
+                {
+                    ProductsAndAmounts = group.Select(x => new OrderLine(x.Amount, new OrderProduct(x.Type)))
+                };
+
+                await supplier.MakeOrder(order);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
